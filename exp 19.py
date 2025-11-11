c9d6cb4157c5c33d9a4e4cd35dc83a8118b2f255
tejareddykr2009-gif <tejareddykr2009@gmail.com>
2025-11-11 10:50:57 +0530

    Add files via upload

---
+++ exp 19.py
@@ -0,0 +1,14 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dec=15
+>>> b=bin(dec)
+>>> o=oct(dec)
+>>> h=hex(dec)
+>>> print("decimal number is : ",dec)
+decimal number is :  15
+>>> print("binary value is : ",b)
+binary value is :  0b1111
+>>> print("octal value is : ",o)
+octal value is :  0o17
+>>> print ("hexadecimal value is : ",h)
+hexadecimal value is :  0xf
